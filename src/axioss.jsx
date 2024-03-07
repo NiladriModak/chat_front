abd17365f90ee41fe1bec81f6ba0d62a9aaae554
NiladriModak <niladrimodakk2001@gmail.com>
2024-03-07 20:19:54 +0530

    connect

--- src/axioss.jsx
+++ src/axioss.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:80",
-  // baseURL: "https://chatapp-backend-2mf4.onrender.com",
+  // baseURL: "http://localhost:80",
+  baseURL: "http://chatapp-backend-2mf4.onrender.com",
 });
 
 export default instance;
